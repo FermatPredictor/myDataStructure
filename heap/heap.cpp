c3fc26538f406097f266acd37bb8081ebb89e0b8
FermatPredictor <forclass021117@gmail.com>
2020-03-30 11:22:07 +0800

    Update heap.cpp
    改成0-index base Heap，建構子改成可以用O(n)時間建heap的heapify()函數了(本來做n次insert需要nlog(n)的時間建heap)

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -2,59 +2,91 @@
 #include <vector>
 using namespace std;
 
-class Heap {
+// 0-index base Heap
+class Heap
+{
 private:
-    vector<int> array = {0}; //初始給一個元素，讓index0的位置閒置
+    void heapify();
 public:
-    Heap() = default;
-    Heap(vector<int> _array);
-    void insert(int num);
+	Heap() = default;
+    Heap(vector<int> vec):arr(vec){heapify();};
+    vector<int> arr;
+    void insert(int data);
     int get_min();
-    int extract_min();
+    void extract_min();
+    void Print();  //印出heap的元素檢查是否滿足規則，debug用
     vector<int> HeapSort();
 };
 
-Heap::Heap(vector<int> _array) {
-    for (int i = 0; i < _array.size(); i++) {
-        insert(_array[i]);
+//調整heap使它滿足parent都比child小的規則(總時間: O(n))
+void Heap::heapify()
+{
+    //這邊與extract-min的做法一樣，從下往上，將有小孩的節點往下交換
+    for (int i = (arr.size()-1)/2; i>=0; i--) {
+        int idx=i;
+        //若節點是有小孩的就繼續迴圈
+        while(idx*2+1 <arr.size()){
+            int small_chlid_idx  = idx*2+1; //判斷小孩節點比較小的編號
+            if (small_chlid_idx + 1 < arr.size() && arr[small_chlid_idx + 1] < arr[small_chlid_idx])
+                small_chlid_idx++;
+            if(arr[idx] > arr[small_chlid_idx])
+                swap(arr[idx], arr[small_chlid_idx]);
+            idx = small_chlid_idx;
+        }
     }
 }
 
-void Heap::insert(int num) {
-    array.push_back(num);
-    int idx = array.size()-1;
-    while (idx / 2 > 0 and array[idx] < array[idx / 2]) {
-        swap(array[idx], array[idx / 2]);
-        idx /= 2;
+void Heap::insert(int data)
+{
+    arr.push_back(data);
+    int idx = arr.size()-1;
+    while(idx>0 && arr[idx]<arr[(idx-1)/2]){
+        swap(arr[idx], arr[(idx-1)/2]);
+        idx = (idx-1)/2;
     }
 }
 
 int Heap::get_min() {
-    return array.size()>1 ? array[1]: 0;
+    return !arr.empty()? arr[0]: 0;
 }
 
-int Heap::extract_min() {
-    int min_element = array[1];
-    int sz = array.size()-1; //元素個數
-    swap(array[1], array[sz]);
-    int idx = 1;
-    while (idx * 2 <sz) {
-        int small_chlid_idx = idx * 2; //判斷小孩節點比較小的那個編號
-        if (small_chlid_idx + 1 < sz && array[small_chlid_idx + 1] < array[small_chlid_idx])
-            small_chlid_idx++; 
-        if(array[idx] > array[small_chlid_idx])
-            swap(array[idx], array[small_chlid_idx]);
+//拿走最小的元素，若heap為空不做事
+void Heap::extract_min()
+{
+    if(arr.empty()){
+        return;
+    }
+    
+    int small = arr[0];
+    swap(arr[0], arr[arr.size()-1]);
+    arr.pop_back(); //清掉最後一個元素(即最小的那個)
+    int idx = 0;
+    //若節點是有小孩的就繼續迴圈
+    while(idx*2+1 <arr.size()){
+        int small_chlid_idx  = idx*2+1; //判斷小孩節點比較小的編號
+        if (small_chlid_idx + 1 < arr.size() && arr[small_chlid_idx + 1] < arr[small_chlid_idx])
+            small_chlid_idx++;
+        if(arr[idx] > arr[small_chlid_idx])
+            swap(arr[idx], arr[small_chlid_idx]);
         idx = small_chlid_idx;
     }
-    array.erase(array.end()-1); //清掉最後一個元素(即最小的那個)
-    return min_element;    
+}
+
+void Heap::Print()
+{
+    for (auto v : arr) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+    
 }
 
 vector<int> Heap::HeapSort() {
-    int sz = array.size() - 1; //元素個數
+    int sz = arr.size(); //元素個數
     vector<int> vec;
     for (int i = 0; i < sz; i++) {
-        vec.push_back(extract_min());
+        vec.push_back(get_min());
+        extract_min();
     }
     return vec;
 }
