735099b040590c6fdbb27e42cd557e531a07f9cc
FermatPredictor <forclass021117@gmail.com>
2020-04-28 18:40:59 +0800

    Update binaryTree_in_pre.cpp

--- problemSet/binaryTree_in_pre.cpp
+++ problemSet/binaryTree_in_pre.cpp
@@ -41,43 +41,56 @@ public:
     Node(int d):data(d), left(NULL), right(NULL){};
 };  
 
-
-//函數功能:利用tree的 inorder 和 preorder 建原來的 tree，記錄當前處理的pre和in的位置
-Node* buildTree(const vector<int> & pre, const vector<int> &in, int pre_left, int pre_right, int in_left, int in_right)  
-{  
- 
-    if (pre_left>pre_right)  
-        return NULL;
-        
-    vector<int> table = transAtoB(pre, in);
-  
-    Node* node = new Node(pre[pre_left]);
-    int inIndex = table[pre_left];
-  
-    node->left = buildTree(pre, in, pre_left+1, pre_left+inIndex-in_left, in_left, inIndex - 1);  
-    node->right = buildTree(pre, in, pre_left+inIndex-in_left+1, pre_right, inIndex + 1, in_right);  
-  
-    return node;  
-}  
-  
- 
-  
-//用來測試建樹是否正確
-void printInorder(Node* node)  
-{  
-    if (node != NULL)  {
-        printInorder(node->left);  
-        cout<<node->data<<" ";  
-        printInorder(node->right);  
+class BinaryTree
+{
+public:
+    Node* root;
+    
+    //函數功能:利用tree的 inorder 和 preorder 建原來的 tree
+    Node* buildTree(const vector<int>& preorder, const vector<int>& inorder) {
+        vector<int> table = transAtoB(preorder, inorder);
+        root = myBuildTree(table, preorder, inorder, 0, preorder.size()-1, 0, inorder.size()-1);
     }
-}  
+    
+    //用來測試建樹是否正確
+    void printInorder(Node* node)  
+    {  
+        if (node != NULL)  {
+            printInorder(node->left);  
+            cout<<node->data<<" ";  
+            printInorder(node->right);  
+        }
+    } 
+
+private:
+    // 函數功能:利用tree的 inorder 和 preorder 建原來的 tree，記錄當前處理的pre和in的位置
+    // 註: 將table當做參數傳進函數，避免遞迴呼叫函數時一直重建table
+    Node* myBuildTree(const vector<int> & table_pre_to_in, const vector<int> & pre, const vector<int> &in, int pre_left, int pre_right, int in_left, int in_right)  
+    {  
+     
+        if (pre_left>pre_right)  
+            return NULL;
+            
+        vector<int> table = transAtoB(pre, in);
+      
+        Node* node = new Node(pre[pre_left]);
+        int inIndex = table_pre_to_in[pre_left];
+      
+        node->left = myBuildTree(table_pre_to_in, pre, in, pre_left+1, pre_left+inIndex-in_left, in_left, inIndex - 1);  
+        node->right = myBuildTree(table_pre_to_in, pre, in, pre_left+inIndex-in_left+1, pre_right, inIndex + 1, in_right);  
+      
+        return node;  
+    }
+    
+};
   
+ 
 
 int main()  
 {  
+    BinaryTree tree;
     vector<int> pre = {3, 9, 20, 15, 7};
     vector<int>  in = {9, 3, 15, 20, 7};
-    Node* root = buildTree(pre, in, 0,pre.size()-1, 0, in.size() - 1); 
-    cout << "樹的inorder為: " << endl;  
-    printInorder(root);  
+    tree.buildTree(pre, in); 
+    tree.printInorder(tree.root); //印出tree的inorder檢查是否正確
 }  
