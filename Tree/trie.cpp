8bb4257a0f5e0f2780f398e7bd70985937e5e9c2
FermatPredictor <forclass021117@gmail.com>
2020-06-08 17:09:34 +0800

    Update trie.cpp

--- Tree/trie.cpp
+++ Tree/trie.cpp
@@ -1,57 +1,87 @@
 #include <string>
 #include <unordered_map>
+#include <vector>
 #include <iostream>
 using namespace std;
 
-class Trie {
-    
-    class TrieNode {
+class Trie
+{
+
+    class TrieNode
+    {
     public:
         unordered_map<char, TrieNode *> child;
+        int leaf_num; // 記錄某點底下有幾個leaf
         bool isWord; //記錄某點是否為單字的結尾
-        TrieNode(): isWord(false){};
+        TrieNode(): leaf_num(0), isWord(false){};
     };
-    
+
     TrieNode* root; //root為一個空的節點
-    
+
 public:
     /** Initialize your data structure here. */
     Trie(): root(new TrieNode()) {};
-    
+
     /** Inserts a word into the trie. */
-    void insert(string word) {
+    void insert(string word)
+    {
         TrieNode *p = root;
-        for (char c : word) {
-            if (p->child.find(c)==p->child.end()){
+        for (char c : word)
+        {
+            if (p->child.find(c)==p->child.end())
+            {
                 p->child[c] = new TrieNode();
-            } 
+            }
+            p ->leaf_num++;
             p = p->child[c];
         }
+        p ->leaf_num++;
         p->isWord = true;
     }
-    
+
     /** Returns if the word is in the trie. */
-    bool search(string word){
+    bool search(string word)
+    {
         TrieNode *p = root;
-        for (char c : word) {
-            if (p->child.find(c)==p->child.end()){
+        for (char c : word)
+        {
+            if (p->child.find(c)==p->child.end())
+            {
                 return false;
             }
             p = p->child[c];
         }
-        return p->isWord;    
+        return p->isWord;
     }
-    
+
     /** Returns if there is any word in the trie that starts with the given prefix. */
-    bool startsWith(string prefix) {
+    bool startsWith(string prefix)
+    {
         TrieNode *p = root;
-        for (char c : prefix) {
-            if (p->child.find(c)==p->child.end()){
+        for (char c : prefix)
+        {
+            if (p->child.find(c)==p->child.end())
+            {
                 return false;
-            } 
+            }
             p = p->child[c];
         }
-        return true;        
+        return true;
+    }
+
+    //回傳有幾個字的開頭是prefix
+    int count_startsWith(string prefix)
+    {
+        TrieNode *p = root;
+        for (char c : prefix)
+        {
+             if (p->child.find(c)==p->child.end())
+            {
+                return false;
+            }
+            p = p->child[c];
+        }
+        return p->leaf_num;
     }
 };
 
